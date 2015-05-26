134a11786c9a47021eb377e2dc0bbfcf06a638a1
Christoffer Holmstedt <christoffer.holmstedt@gmail.com>
2015-05-26 09:45:01 +0200

    initial commit: add vagrant settings and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+A default Vagrant machine
+=========================
+This is a template project with my common Vagrant settings scripts that can
+easily be copied to new projects when I start them.
---
+++ scripts/install-required-packages.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#############################################
+# Installation of required packages for
+# gcc compilation and some helper tools
+#############################################
+
+### Basic everyday utilities
+sudo apt-get -y install vim git mutt procmail
+
+### Build tools
+sudo apt-get -y install build-essential cmake doxygen
